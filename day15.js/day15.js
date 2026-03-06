9dbc2e36823a3b14588039882c5bbf7e6106e86f
Your Name <your@email.com>
2026-03-06 18:35:00 +0530

    add msg

---
+++ day15.js/day15.js
@@ -0,0 +1,53 @@
+// callback hell
+
+//setTimeout(() => {
+//    console.log("1");
+//    setTimeout(() => {
+//        console.log("2");
+//        setTimeout(() => {
+//            console.log("3");
+//            setTimeout(() => {
+//                console.log("4");
+//            }, 1000);
+//        }, 1000);
+//    }, 1000);
+//}, 1000);
+
+
+// promise
+//function printAfterDelay(num, delay) {
+//    return new Promise((resolve) => {
+//        setTimeout(() => {
+//            console.log(num);
+//            resolve();
+//        }, delay);
+//    });
+//}
+
+//promise 
+
+//printAfterDelay(1, 1000)
+//    .then(() => printAfterDelay(2, 1000))
+//    .then(() => printAfterDelay(3, 1000))
+//    .then(() => printAfterDelay(4, 1000));
+
+// promise consuers
+//1. .then(): when promise resolved it will go to .then
+//2. .catch(): when promise rejected it will go to .catch
+
+function pro() {
+    return new Promise((resolve, rejected) => {
+        let payment = "True";
+        if (payment == "True") {
+            resolve("Dashboard Accessed");
+        } else {
+            rejected("Dashboard Access Denied");
+        }
+    })
+}
+
+pro().then((message) => {
+    console.log(message);
+}).catch((error) => {
+    console.error(error);
+});
